5f2ed0d5a400e527b377c79575101c7cc09bf546
Evan <daley.evan@gmail.com>
2026-01-21 18:00:23 -0800

    initial commmit

---
+++ src/stores/sceneStore.js
@@ -0,0 +1,32 @@
+import create from 'zustand'
+// import ConnectPage from "../modules/networking_focus/connect_page/ConnectPage";
+// import ConnectPageOverlay from "../modules/networking_focus/connect_page/ConnectPageOverlay";
+// import LandingArea from "../modules/simple_playable_areas/LandingArea";
+import Courtyard from "../modules/courtyard/Courtyard";
+
+const scenes = [
+  // { id: 'connectPage', name: 'Join Game', scene: ConnectPage, overlay: ConnectPageOverlay },
+  { id: 'courtyard', name: 'Courtyard', scene: Courtyard },
+  // { id: 'lobby', name: 'Lobby', scene: Lobby },
+  // { id: 'office', name: 'Office', scene: Office },
+  // { id: 'hallway', name: 'Hallway', scene: Hallway },
+  // { id: 'bathroom', name: 'Bathroom', scene: Bathroom },
+  // { id: 'kitchen', name: 'Kitchen', scene: Kitchen },
+  // { id: 'livingRoom', name: 'Living Room', scene: LivingRoom },
+  // { id: 'bedroom', name: 'Bedroom', scene: Bedroom },
+]
+
+let defaultScene = 'courtyard'
+
+// On the prod version, always default to connectPage. When testing locally,
+if (window.location.hostname !== 'localhost') {
+  defaultScene = 'courtyard';
+}
+
+const useSceneStore = create((set) => ({
+  currentSceneId: defaultScene,
+  scenes,
+  setSceneId: (id) => set({ currentSceneId: id }),
+}))
+
+export default useSceneStore
